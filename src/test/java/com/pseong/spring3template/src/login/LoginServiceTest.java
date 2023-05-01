a5e97ef2e5a0667266f295c3a1b1aec9ac1b34d7
SeongUk <pkseonguk@gmail.com>
2023-05-02 03:26:10 +0900

    Add Test

--- src/test/java/com/pseong/spring3template/src/login/LoginServiceTest.java
+++ src/test/java/com/pseong/spring3template/src/login/LoginServiceTest.java
@@ -1,4 +1,24 @@
+package com.pseong.spring3template.src.login;
+
+import com.pseong.spring3template.config.BaseException;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class LoginServiceTest {
-  
+
+    @Autowired
+    LoginService loginService;
+
+    @Test
+    void 아이디비밀번호_유저생성_테스트() throws Exception {
+        String username = "pseong";
+        String password = "mypasword1234@";
+        loginService.createUser(username, password);
+        loginService.loginPassword(username, password);
+    }
 }

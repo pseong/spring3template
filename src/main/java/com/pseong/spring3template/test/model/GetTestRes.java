2df545759a97fd32d2c0ff4a3569017abd53c644
SeongUk <pkseonguk@gmail.com>
2023-05-01 12:30:55 +0900

    Add TestContoller for test BaseResponse

---
+++ src/main/java/com/pseong/spring3template/test/model/GetTestRes.java
@@ -0,0 +1,12 @@
+package com.pseong.spring3template.test.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GetTestRes {
+    private String name;
+}

8477c9ae90f48c84c19a695d629e5f8129356487
SeongUk <pkseonguk@gmail.com>
2023-05-01 11:56:45 +0900

    Init project

---
+++ src/main/java/com/pseong/spring3template/Spring3templateApplication.java
@@ -0,0 +1,13 @@
+package com.pseong.spring3template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3templateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3templateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pseong/spring3template/Spring3templateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pseong.spring3template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3templateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

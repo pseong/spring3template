05aee6930346b1d86a94524f43bb7c5b19e7bf3e
SeongUk <pkseonguk@gmail.com>
2023-05-02 03:25:46 +0900

    Add kakao, apple, google type in constant

--- src/main/java/com/pseong/spring3template/config/Constant.java
+++ src/main/java/com/pseong/spring3template/config/Constant.java
@@ -7,6 +7,9 @@ import java.util.List;
 // 프로젝트에서 공통적으로 사용하는 상수들
 public class Constant {
     public static final String GOOGLE_TOKEN_BASE_URL = "https://oauth2.googleapis.com/tokeninfo";
+    public static final Long GOOGLE_TYPE = 1L;
+    public static final Long APPLE_TYPE = 2L;
+    public static final Long KAKAO_TYPE = 3L;
     public static final String[][] WHITE_LIST_URI_EQUAL = {
             {"/user/login", "POST"},
             {"/test/login", "GET"}
